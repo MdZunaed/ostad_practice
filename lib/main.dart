6e02ea863e6c81bcb7ab60b3900664305a665924
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2023-09-24 23:26:38 +0600

    module 7 live test

--- lib/main.dart
+++ lib/main.dart
@@ -11,90 +11,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ProductList(),
+      home: CounterApp(),
     );
   }
 }
 
-class ProductList extends StatefulWidget {
-  const ProductList({Key? key}) : super(key: key);
+class CounterApp extends StatefulWidget {
+  const CounterApp({Key? key}) : super(key: key);
 
   @override
-  State<ProductList> createState() => _ProductListState();
+  State<CounterApp> createState() => _CounterAppState();
 }
+int count = 0;
 
-class _ProductListState extends State<ProductList> {
-  Map<int,int> selectedProducts = {};
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text("Product List")),
-      body: ListView.builder(
-          itemCount: 15,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text("Product ${index + 1}"),
-              subtitle: const Text("\$10.50"),
-              trailing: FittedBox(
-                child: Column(
-                  children: [
-                    Text("Counts: ${selectedProducts[index]??0}"),
-                    ElevatedButton(
-                        onPressed: () {
-                          if(selectedProducts.containsKey(index)){
-                            selectedProducts[index] = selectedProducts[index]! + 1;
-                          } else{
-                            selectedProducts[index] = 1;
-                          }
-                          if (selectedProducts[index] == 5) {
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title:const Text('Congratulations!'),
-                                  content: Text('You\'ve bought 5 Product $index!'),
-                                  actions: <Widget>[
-                                    TextButton(
-                                      child:const Text('OK'),
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
-                                );
-                              },
-                            );
-                          }
-                          setState(() {});
-                        },
-                        child: const Text("Buy Now"))
-                  ],
-                ),
-              ),
-            );
-          }),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            int totalProduct = selectedProducts.length;
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>CartPage(totalProductCount: totalProduct)));
-          }, child: const Icon(Icons.shopping_cart)),
-    );
-  }
-}
-
-class CartPage extends StatelessWidget {
-  final int totalProductCount;
-  const CartPage({Key? key,required this.totalProductCount}) : super(key: key);
-
+class _CounterAppState extends State<CounterApp> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title:const Text("Cart")),
-      body: Center(child:
-        Text("Total Product: $totalProductCount")),
+      appBar: AppBar(title:const Text("Counter App")),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text("Count:",style: TextStyle(fontSize: 20)),
+          Text(count.toString(),style:const TextStyle(fontSize: 50)),
+          const SizedBox(height: 10),
+          Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: (){
+                count++;
+                if(count >4){
+                  showDialog(context: context, builder: (context){
+                    return AlertDialog(
+                      content: Text("Button pressed $count times"),
+                      actions: [TextButton(onPressed: (){
+                        Navigator.pop(context);
+                      }, child:const Text("Close"))],
+                    );
+                  });
+                }
+                setState(() {});
+              }, child:const Icon(Icons.add)),
+              const SizedBox(width: 20),
+              ElevatedButton(onPressed: (){
+                if(count > 0){
+                  count--;
+                }
+                setState(() {});
+              }, child:const Icon(Icons.remove)),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
-
