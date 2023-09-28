41581644e9b87b9177f02437f653294bd3d98afb
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2023-09-28 22:16:41 +0600

    module 8 assignment

--- lib/main.dart
+++ lib/main.dart
@@ -11,57 +11,67 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterApp(),
+      home: HomePage(),
     );
   }
 }
 
-class CounterApp extends StatefulWidget {
-  const CounterApp({Key? key}) : super(key: key);
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
-  @override
-  State<CounterApp> createState() => _CounterAppState();
-}
-int count = 0;
-
-class _CounterAppState extends State<CounterApp> {
   @override
   Widget build(BuildContext context) {
+    String title = "John Doe";
+    String description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
+    String circleImage = "https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?resize=1200:*";
+    String squareImage = "https://www.kitnipbox.com/meow/wp-content/uploads/2020/06/HowtoBondwithYourKitten-1024x765.jpg";
     return Scaffold(
-      appBar: AppBar(title:const Text("Counter App")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Text("Count:",style: TextStyle(fontSize: 20)),
-          Text(count.toString(),style:const TextStyle(fontSize: 50)),
-          const SizedBox(height: 10),
-          Row(mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(onPressed: (){
-                count++;
-                if(count >4){
-                  showDialog(context: context, builder: (context){
-                    return AlertDialog(
-                      content: Text("Button pressed $count times"),
-                      actions: [TextButton(onPressed: (){
-                        Navigator.pop(context);
-                      }, child:const Text("Close"))],
-                    );
-                  });
-                }
-                setState(() {});
-              }, child:const Icon(Icons.add)),
-              const SizedBox(width: 20),
-              ElevatedButton(onPressed: (){
-                if(count > 0){
-                  count--;
-                }
-                setState(() {});
-              }, child:const Icon(Icons.remove)),
-            ],
-          ),
-        ],
+      appBar: AppBar(title:const Text("Profile")),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: OrientationBuilder(
+          builder: (context, orientation) {
+            return (orientation==Orientation.portrait)? Column(crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                CircleAvatar(radius: 80,backgroundImage: NetworkImage(circleImage)),
+                const SizedBox(height: 20),
+                Text(title,style:const TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                const SizedBox(height: 20),
+                Text(description),
+                const SizedBox(height: 20),
+                Expanded(
+                  child: GridView.builder(
+                      gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,crossAxisSpacing: 10,mainAxisSpacing: 10,childAspectRatio: 1),
+                      itemCount: 9,
+                      itemBuilder: (context, index){
+                    return Image.network(squareImage,fit: BoxFit.cover);
+                  }),
+                )
+              ],
+            ):Row(
+              children: [
+                Expanded(flex: 1,child: CircleAvatar(radius: 80,backgroundImage: NetworkImage(circleImage))),
+                Expanded(flex: 2,child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(title,style:const TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
+                    const SizedBox(height: 20),
+                    Text(description),
+                    const SizedBox(height: 20),
+                    Expanded(
+                      child: GridView.builder(
+                          gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,crossAxisSpacing: 10,mainAxisSpacing: 10),
+                          itemCount: 9,
+                          itemBuilder: (context, index){
+                            return Image.network(squareImage,fit: BoxFit.cover);
+                       }),
+                    )
+                  ],
+                )),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
--- practice.dart
+++
@@ -1,6 +0,0 @@
-
-void main() {
-  List name = ["Mango", "Banana", "Apple", "Cherry"];
-  name.add("strawberry");
-  print(name);
-}
