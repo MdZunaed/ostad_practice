32964e5b963fa921c9b6faa93143aac7d9f546e0
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2023-09-29 23:18:16 +0600

    module 8 live test

--- lib/main.dart
+++ lib/main.dart
@@ -21,55 +21,51 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    String title = "John Doe";
-    String description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
-    String circleImage = "https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?resize=1200:*";
-    String squareImage = "https://www.kitnipbox.com/meow/wp-content/uploads/2020/06/HowtoBondwithYourKitten-1024x765.jpg";
+    String image = "https://via.placeholder.com/150";
     return Scaffold(
-      appBar: AppBar(title:const Text("Profile")),
+      appBar: AppBar(title: const Text("News Feed")),
       body: Padding(
         padding: const EdgeInsets.all(10),
         child: OrientationBuilder(
           builder: (context, orientation) {
-            return (orientation==Orientation.portrait)? Column(crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                CircleAvatar(radius: 80,backgroundImage: NetworkImage(circleImage)),
-                const SizedBox(height: 20),
-                Text(title,style:const TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                const SizedBox(height: 20),
-                Text(description),
-                const SizedBox(height: 20),
-                Expanded(
-                  child: GridView.builder(
-                      gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,crossAxisSpacing: 10,mainAxisSpacing: 10,childAspectRatio: 1),
-                      itemCount: 9,
-                      itemBuilder: (context, index){
-                    return Image.network(squareImage,fit: BoxFit.cover);
-                  }),
-                )
-              ],
-            ):Row(
-              children: [
-                Expanded(flex: 1,child: CircleAvatar(radius: 80,backgroundImage: NetworkImage(circleImage))),
-                Expanded(flex: 2,child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(title,style:const TextStyle(fontSize: 20,fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 20),
-                    Text(description),
-                    const SizedBox(height: 20),
+            return (orientation == Orientation.portrait)
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                        Expanded(
+                          child: ListView.builder(
+                              itemCount: 15,
+                              itemBuilder: (context, index) {
+                                return Card(
+                                  child: Image.network(image,
+                                      height: 150,
+                                      width: 150,
+                                      fit: BoxFit.scaleDown),
+                                );
+                              }),
+                        )
+                      ])
+                : Row(children: [
                     Expanded(
                       child: GridView.builder(
-                          gridDelegate:const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3,crossAxisSpacing: 10,mainAxisSpacing: 10),
-                          itemCount: 9,
-                          itemBuilder: (context, index){
-                            return Image.network(squareImage,fit: BoxFit.cover);
-                       }),
-                    )
-                  ],
-                )),
-              ],
-            );
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing: 10,
+                                  mainAxisSpacing: 10),
+                          itemCount: 15,
+                          itemBuilder: (context, index) {
+                            return Card(
+                              child: Image.network(
+                                image,
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.scaleDown,
+                              ),
+                            );
+                          }),
+                    ),
+                  ]);
           },
         ),
       ),
