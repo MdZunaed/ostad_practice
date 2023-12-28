a66fd0c63ced6d80996ee543a7e71dffce5c0efe
Zunayed Ebney Faruk <122368527+MdZunaed@users.noreply.github.com>
2023-12-28 13:30:37 +0600

    Firebase FCM Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:ostad_practice/services/firebase_messaging.dart';
 import 'screen/home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseMessagingServices().initialize();
+  await FirebaseMessagingServices().getFCMToken();
+  await FirebaseMessagingServices().subscribeToTopic("demo");
+  await FirebaseMessagingServices().onRefresh((token) {
+    // TODO - Send to backend
+  });
   runApp(const MyApp());
 }
 
--- lib/services/firebase_messaging.dart
+++ lib/services/firebase_messaging.dart
@@ -17,16 +17,40 @@ class FirebaseMessagingServices {
 
   Future<void> initialize() async {
     await requestPermission();
+    // Normal / Alive / On-pause
     FirebaseMessaging.onMessage.listen((message) {
       print(message.notification!.title!);
       print(message.notification!.body!);
       print(message.data);
     });
+    // Open / Resume
     FirebaseMessaging.onMessageOpenedApp.listen((message) {
       print(message.notification!.title!);
       print(message.notification!.body!);
       print(message.data);
     });
-    // FirebaseMessaging.onBackgroundMessage(BackgroundMessageHandler);
+    // Dead / On-Terminated
+    FirebaseMessaging.onBackgroundMessage(handleBackgroundNotification);
+  }
+
+  Future<void> getFCMToken() async {
+    final token = await firebaseMessaging.getToken();
+    print(token);
+  }
+
+  Future<void> onRefresh(Function(String) onTokenRefresh) async {
+    firebaseMessaging.onTokenRefresh.listen((token) {
+      onTokenRefresh(token);
+    });
+  }
+
+  Future<void> subscribeToTopic(String topic) async {
+    await firebaseMessaging.subscribeToTopic(topic);
+  }
+
+  Future<void> unSubscribeToTopic(String topic) async {
+    await firebaseMessaging.unsubscribeFromTopic(topic);
   }
 }
+
+Future<void> handleBackgroundNotification(RemoteMessage message) async {}
